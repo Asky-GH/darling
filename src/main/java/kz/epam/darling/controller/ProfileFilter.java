8d1a1cd05605deacbca4ebf54f0ac22e9292e571
Askhat Abishev <asky.gm@gmail.com>
2019-01-24 01:40:11 +0600

    add filter to '/profile' route

---
+++ src/main/java/kz/epam/darling/controller/ProfileFilter.java
@@ -0,0 +1,32 @@
+package kz.epam.darling.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class ProfileFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("user") == null) {
+            HttpServletResponse response = (HttpServletResponse) servletResponse;
+            response.sendRedirect(request.getContextPath() + "/");
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+
+    }
+}
