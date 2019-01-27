8d6828d6563df320fd07b44c3269fffcae7e74d5
Askhat Abishev <asky.gm@gmail.com>
2019-01-27 23:55:46 +0600

    rename application servlet

---
+++ src/main/java/kz/epam/darling/controller/DarlingServlet.java
@@ -0,0 +1,21 @@
+package kz.epam.darling.controller;
+
+import kz.epam.darling.controller.command.CommandFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DarlingServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        CommandFactory.getInstance().getCommand(req.getServletPath()).execute(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
